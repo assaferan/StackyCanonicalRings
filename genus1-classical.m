1f1346f2496c0d0ad6a7d8923f8c2f8ca66efc06
Sam Frengley <sam.frengley@gmail.com>
2023-06-06 11:01:54 -0400

    genus 1 base done

--- genus1-classical.m
+++ genus1-classical.m
@@ -17,20 +17,29 @@ intrinsic GenusOnePointedGenericInitialIdeal(deg_delta)
     ZZ := Integers(); //base field, one can change.
     
     if deg_delta eq 1 then
+        // Elliptic curve with Weiertsrass equation
+        
          P<[x]> := PolynomialRing(ZZ, [3,2,1]);
          return [x[1]^2];
 
     elif deg_delta eq 2 then
+        // A degree 2 genus one curve (double cover of P1 ramified over 4 points)
+        
         P<[x]> := PolynomialRing(ZZ, [2,1,1]);
         return [x[1]^2];
 
     elif deg_delta eq 3 then
+        // A degree 3 genus one curve (plane cubic)
+
         P<[x]> := PolynomialRing(ZZ, [1,1,1]);
         return [x[1]^3];
-
     else
-        return 0;
-
+        // An n-covering, genus one normal curve
+        
+        P<[x]> := PolynomialRing(ZZ, [1 : 1 in [1..deg_delta]]);
+        l1 := [[x[i]*x[j] : i in [1..j-1] | <i,j> ne <deg_delta-2, deg_delta-1> ] : j in 1..deg_delta-1];
+        l2 := [x[deg_delta-2]^2*x[deg_delta-1]];
+        return l1 cat l2;
     end if;
 
 end intrinsic;
